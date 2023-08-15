4800c42356efd149c9b3e8ae741e2edd361c2145
Raboro <marius.woerfelreal@gmail.com>
2023-08-15 12:58:22 +0200

    fetched all required values

--- src/main.c
+++ src/main.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 #include <getopt.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
 
 void printHelp() {
     printf("Usage: ns [OPTIONS] [SOURCE_BASE] [TARGET_BASES] [NUMBER]\n");
@@ -15,16 +18,62 @@ int printErrorInvalidInput() {
     return 1;
 }
 
+int countTargetBasis(const char *str) {
+    int count = 0;
+    while (*str) {
+        if (*str == ',') {
+            count++;
+        }
+        str++;
+    }
+    return count;
+}
+
 int action(int argc, char* argv[], struct option* options) {
     int opt;
+    int base;
+    char* endPtr;
+    int targetBases[countTargetBasis(argv[4])];
+    int numTargetBases = 0;
+    const char *charNumber;
+
     while ((opt = getopt_long(argc, argv, "hi:t:n:", options, NULL)) != -1) {
         switch (opt) {
             case 'h':
                 printHelp();
                 return 0;
+            case 'i':
+                base = strtol(optarg, &endPtr, 10);
+                if (*endPtr != '\0') {
+                    fprintf(stderr, "Invalid source base.\n");
+                    return 1;
+                }
+                break;
+            case 't': {
+                char *targetBaseToken = strtok(optarg, ",");
+                while (targetBaseToken != NULL) {
+                    targetBases[numTargetBases++] = strtol(targetBaseToken, &endPtr, 10);
+                    if (*endPtr != '\0') {
+                        fprintf(stderr, "Invalid target base.\n");
+                        return 1;
+                    }
+                    targetBaseToken = strtok(NULL, ",");
+                }
+                break;
+            }
+            case 'n':
+                charNumber = optarg;
+                break;
+            default:
+                printErrorInvalidInput();
         }
     }
-    return printErrorInvalidInput();
+    const double number = strtod(charNumber, NULL);
+    if (base == 0 || numTargetBases == 0 || isnan(number)) {
+        fprintf(stderr, "Missing required arguments.\n");
+        return 1;
+    }
+    return 0;
 }
 
 int main(int argc, char* argv[]) {
