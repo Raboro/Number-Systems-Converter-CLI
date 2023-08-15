1ad235267fd32288a0e91565cb4218512945bb32
Raboro <marius.woerfelreal@gmail.com>
2023-08-15 09:25:23 +0200

    added printHelp with the explanation of the tool

--- src/main.c
+++ src/main.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 
+void printHelp() {
+    printf("Usage: ns [OPTIONS] [SOURCE_BASE] [TARGET_BASES] [NUMBER]\n");
+    printf("Options:\n");
+    printf("  -h, --help               Display this help message\n");
+    printf("  -i, --input-base BASE    Source number base\n");
+    printf("  -t, --target-bases BASES Space-separated list of target bases\n");
+    printf("  -n, --number NUMBER      Number to convert\n");
+}
+
 int main() {
-    printf("Hello, World!\n");
+    printHelp();
     return 0;
 }
